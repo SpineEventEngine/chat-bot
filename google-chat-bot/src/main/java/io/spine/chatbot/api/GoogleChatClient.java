4d253dcce32b00a5968a641fa124275a53fcff3d
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-06-01 18:17:43 +0300

    Create stub client

---
+++ google-chat-bot/src/main/java/io/spine/chatbot/api/GoogleChatClient.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.chatbot.api;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.chat.v1.HangoutsChat;
+import com.google.api.services.chat.v1.model.Message;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+
+public final class GoogleChatClient {
+
+    private static final String BOT_NAME = "Spine Chat Bot";
+    private static final String CHAT_BOT_SCOPE = "https://www.googleapis.com/auth/chat.bot";
+
+    private GoogleChatClient() {
+    }
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        var keyStream = Files.newInputStream(Paths.get("spine-chat-bot-ea2e6c200084.json"));
+
+        var credentials = GoogleCredentials.fromStream(keyStream)
+                                           .createScoped(CHAT_BOT_SCOPE);
+        var credentialsAdapter = new HttpCredentialsAdapter(credentials);
+        var chat = new HangoutsChat.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(),
+                credentialsAdapter)
+                .setApplicationName(BOT_NAME)
+                .build();
+        chat.spaces()
+            .list()
+            .execute()
+            .getSpaces()
+            .forEach(System.out::println);
+        var message = new Message()
+                .setName("test-message")
+                .setText("This is a test message from the Bot API.");
+        System.out.println(chat.spaces()
+                               .messages()
+                               .create("", message)
+                               .execute());
+    }
+}
