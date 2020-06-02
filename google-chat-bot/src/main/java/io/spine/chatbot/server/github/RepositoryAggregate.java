76d15ac06e0b93738c018a6d4d62c1d89cc60778
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-06-02 17:45:04 +0300

    Define repository aggregate

---
+++ google-chat-bot/src/main/java/io/spine/chatbot/server/github/RepositoryAggregate.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.chatbot.server.github;
+
+import io.spine.chatbot.github.BuildStateChange;
+import io.spine.chatbot.github.Repository;
+import io.spine.chatbot.github.RepositoryId;
+import io.spine.chatbot.github.repository.command.RegisterRepository;
+import io.spine.chatbot.github.repository.command.SetBuildState;
+import io.spine.chatbot.github.repository.event.BuildStateChanged;
+import io.spine.chatbot.github.repository.event.RepositoryRegistered;
+import io.spine.server.aggregate.Aggregate;
+import io.spine.server.aggregate.Apply;
+import io.spine.server.command.Assign;
+
+final class RepositoryAggregate
+        extends Aggregate<RepositoryId, Repository, Repository.Builder> {
+
+    @Assign
+    RepositoryRegistered handle(RegisterRepository c) {
+        var result = RepositoryRegistered
+                .newBuilder()
+                .setId(c.getId())
+                .setName(c.getName())
+                .setGithubUrl(c.getGithubUrl())
+                .setTravisCiUrl(c.getTravisCiUrl())
+                .vBuild();
+        return result;
+    }
+
+    @Apply
+    private void on(RepositoryRegistered e) {
+        builder().setName(e.getName())
+                 .setGithubUrl(e.getGithubUrl())
+                 .setTravisCiUrl(e.getTravisCiUrl());
+    }
+
+    @Assign
+    BuildStateChanged handle(SetBuildState c) {
+        var stateChange = BuildStateChange
+                .newBuilder()
+                .setPreviousValue(state().getBuildState())
+                .setNewValue(c.getBuildState())
+                .vBuild();
+        var result = BuildStateChanged
+                .newBuilder()
+                .setId(c.getId())
+                .setChange(stateChange)
+                .vBuild();
+        return result;
+    }
+
+    @Apply
+    private void on(BuildStateChanged e) {
+        builder().setBuildState(e.getChange()
+                                 .getNewValue());
+    }
+}
