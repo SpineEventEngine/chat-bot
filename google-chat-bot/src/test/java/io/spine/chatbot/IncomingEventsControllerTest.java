339719d68c7ad9238416d00120ca4c38d50eed08
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-06-11 19:28:26 +0300

    Fix test JSON

--- google-chat-bot/src/test/java/io/spine/chatbot/IncomingEventsControllerTest.java
+++ google-chat-bot/src/test/java/io/spine/chatbot/IncomingEventsControllerTest.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2020, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package io.spine.chatbot;
 
 import com.google.protobuf.ByteString;
@@ -40,9 +60,6 @@ final class IncomingEventsControllerTest {
     @Test
     @DisplayName("receive and decode a pubsub message with Google Chat event")
     void receiveAndDecode() {
-        //TODO:2020-06-11:ysergiichuk: make sure we handle serialization/deserialization of the
-        // chat events properly. Looks like the JSON below could not be deserialized due to internal
-        // proto issue
         var pubsubMessage = PubsubMessage
                 .newBuilder()
                 .setMessageId("129y418y4houfhiuehwr")
@@ -57,7 +74,7 @@ final class IncomingEventsControllerTest {
                 .contentType(MediaType.APPLICATION_JSON);
         String actual = client.toBlocking()
                               .retrieve(request);
-        assertEquals("success", actual);
+        assertEquals("OK", actual);
     }
 
     private static ByteString base64Encode(String value) {
@@ -67,49 +84,49 @@ final class IncomingEventsControllerTest {
 
     private static final String CHAT_MESSAGE_EVENT = "" +
             "{\n" +
-            "  \"type\": \"MESSAGE\",\n" +
-            "  \"eventTime\": \"2017-03-02T19:02:59.910959Z\",\n" +
-            "  \"space\": {\n" +
-            "    \"name\": \"spaces/AAAAAAAAAAA\",\n" +
-            "    \"displayName\": \"Chuck Norris Discussion Room\",\n" +
-            "    \"type\": \"ROOM\"\n" +
+            "  \"type\":\"MESSAGE\",\n" +
+            "  \"eventTime\":\"2017-03-02T19:02:59.910959Z\",\n" +
+            "  \"space\":{\n" +
+            "    \"name\":\"spaces/AAAAAAAAAAA\",\n" +
+            "    \"displayName\":\"Chuck Norris Discussion Room\",\n" +
+            "    \"type\":\"ROOM\"\n" +
             "  },\n" +
-            "  \"message\": {\n" +
-            "    \"name\": \"spaces/AAAAAAAAAAA/messages/CCCCCCCCCCC\",\n" +
-            "    \"sender\": {\n" +
-            "      \"name\": \"users/12345678901234567890\",\n" +
-            "      \"displayName\": \"Chuck Norris\",\n" +
-            "      \"avatarUrl\": \"https://lh3.googleusercontent.com/.../photo.jpg\",\n" +
-            "      \"email\": \"chuck@example.com\"\n" +
+            "  \"message\":{\n" +
+            "    \"name\":\"spaces/AAAAAAAAAAA/messages/CCCCCCCCCCC\",\n" +
+            "    \"sender\":{\n" +
+            "      \"name\":\"users/12345678901234567890\",\n" +
+            "      \"displayName\":\"Chuck Norris\",\n" +
+            "      \"avatarUrl\":\"https://lh3.googleusercontent.com/.../photo.jpg\",\n" +
+            "      \"email\":\"chuck@example.com\"\n" +
             "    },\n" +
-            "    \"createTime\": \"2017-03-02T19:02:59.910959Z\",\n" +
-            "    \"text\": \"@TestBot Violence is my last option.\",\n" +
-            "    \"argumentText\": \" Violence is my last option.\",\n" +
-            "    \"thread\": {\n" +
-            "      \"name\": \"spaces/AAAAAAAAAAA/threads/BBBBBBBBBBB\"\n" +
+            "    \"createTime\":\"2017-03-02T19:02:59.910959Z\",\n" +
+            "    \"text\":\"@TestBot Violence is my last option.\",\n" +
+            "    \"argumentText\":\" Violence is my last option.\",\n" +
+            "    \"thread\":{\n" +
+            "      \"name\":\"spaces/AAAAAAAAAAA/threads/BBBBBBBBBBB\"\n" +
             "    },\n" +
-            "    \"annotations\": [\n" +
+            "    \"annotations\":[\n" +
             "      {\n" +
-            "        \"length\": 8,\n" +
-            "        \"startIndex\": 0,\n" +
-            "        \"userMention\": {\n" +
-            "          \"type\": \"MENTION\",\n" +
-            "          \"user\": {\n" +
-            "            \"avatarUrl\": \"https://.../avatar.png\",\n" +
-            "            \"displayName\": \"TestBot\",\n" +
-            "            \"name\": \"users/1234567890987654321\",\n" +
-            "            \"type\": \"BOT\"\n" +
+            "        \"length\":8,\n" +
+            "        \"startIndex\":0,\n" +
+            "        \"userMention\":{\n" +
+            "          \"type\":\"MENTION\",\n" +
+            "          \"user\":{\n" +
+            "            \"avatarUrl\":\"https://.../avatar.png\",\n" +
+            "            \"displayName\":\"TestBot\",\n" +
+            "            \"name\":\"users/1234567890987654321\",\n" +
+            "            \"type\":\"BOT\"\n" +
             "          }\n" +
             "        },\n" +
-            "        \"type\": \"USER_MENTION\"\n" +
+            "        \"type\":\"USER_MENTION\"\n" +
             "      }\n" +
-            "    ],\n" +
+            "    ]\n" +
             "  },\n" +
-            "  \"user\": {\n" +
-            "    \"name\": \"users/12345678901234567890\",\n" +
-            "    \"displayName\": \"Chuck Norris\",\n" +
-            "    \"avatarUrl\": \"https://lh3.googleusercontent.com/.../photo.jpg\",\n" +
-            "    \"email\": \"chuck@example.com\"\n" +
+            "  \"user\":{\n" +
+            "    \"name\":\"users/12345678901234567890\",\n" +
+            "    \"displayName\":\"Chuck Norris\",\n" +
+            "    \"avatarUrl\":\"https://lh3.googleusercontent.com/.../photo.jpg\",\n" +
+            "    \"email\":\"chuck@example.com\"\n" +
             "  }\n" +
             "}";
 }
