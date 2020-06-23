f065a46fe6eb68221ebfa8409ef7f61aa9f6d193
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-06-23 11:28:34 +0300

    Move docs to the class level and extract help methods.

--- google-chat-bot/src/main/java/io/spine/chatbot/server/google/chat/IncomingEventsHandler.java
+++ google-chat-bot/src/main/java/io/spine/chatbot/server/google/chat/IncomingEventsHandler.java
@@ -31,62 +31,43 @@ import io.spine.server.event.React;
 import io.spine.server.model.Nothing;
 import io.spine.server.tuple.EitherOf4;
 
+import static io.spine.chatbot.server.google.chat.GoogleChatIdentifier.message;
 import static io.spine.chatbot.server.google.chat.GoogleChatIdentifier.space;
 
 /**
  * Google Chat incoming events reactor.
  *
- * <p>Processes {@link ChatEvent}s received by the ChatBot.
+ * <p>Processes incoming {@link ChatEvent} message and emits one of the following domain events:
+ *
+ * <ul>
+ *     <li>{@link BotAddedToSpace} — the ChatBot is added to a Google Chat space;
+ *     <li>{@link BotRemovedFromSpace} — the ChatBot is removed from the Google Chat space;
+ *     <li>{@link MessageReceived} — the ChatBot received a new incoming message from a user
+ *     within a Google Chat space.
+ * </ul>
+ *
+ * <p>If the bot receives a chat event with a not supported currently event type,
+ * {@link Nothing} is emitted.
  */
 final class IncomingEventsHandler extends AbstractEventReactor implements Logging {
 
-    /**
-     * Processes incoming {@link ChatEvent} message and emits one of the following domain events:
-     *
-     * <ul>
-     *     <li>{@link BotAddedToSpace} — the ChatBot is added to a Google Chat space;
-     *     <li>{@link BotRemovedFromSpace} — the ChatBot is removed from the Google Chat space;
-     *     <li>{@link MessageReceived} — the ChatBot received a new incoming message from a user
-     *     within a Google Chat space.
-     * </ul>
-     *
-     * <p>If the bot receives a chat event with a not supported currently event type,
-     * {@link Nothing} is emitted.
-     */
     @React
     EitherOf4<BotAddedToSpace, BotRemovedFromSpace, MessageReceived, Nothing>
     on(@External ChatEvent chatEvent) {
         switch (chatEvent.getType()) {
             case MESSAGE:
                 _info().log("New user message received.");
-                var message = chatEvent.getMessage();
-                var messageId = GoogleChatIdentifier.message(message.getName());
-                var messageReceived = MessageReceived
-                        .newBuilder()
-                        .setEvent(chatEvent)
-                        .setMessageId(messageId)
-                        .vBuild();
-                return EitherOf4.withC(messageReceived);
+                return EitherOf4.withC(messageReceived(chatEvent));
             case ADDED_TO_SPACE:
                 var toSpace = chatEvent.getSpace();
                 _info().log("ChatBot added to space `%s` (%s).",
                             toSpace.getDisplayName(), toSpace.getName());
-                var addedToSpace = BotAddedToSpace
-                        .newBuilder()
-                        .setEvent(chatEvent)
-                        .setSpaceId(space(toSpace.getName()))
-                        .vBuild();
-                return EitherOf4.withA(addedToSpace);
+                return EitherOf4.withA(botAddedToSpace(chatEvent));
             case REMOVED_FROM_SPACE:
                 var fromSpace = chatEvent.getSpace();
                 _info().log("ChatBot removed from space `%s` (%s).",
                             fromSpace.getDisplayName(), fromSpace.getName());
-                var removedFromSpace = BotRemovedFromSpace
-                        .newBuilder()
-                        .setEvent(chatEvent)
-                        .setSpaceId(space(fromSpace.getName()))
-                        .vBuild();
-                return EitherOf4.withB(removedFromSpace);
+                return EitherOf4.withB(botRemovedFromSpace(chatEvent));
 
             case CARD_CLICKED:
             case UNRECOGNIZED:
@@ -95,5 +76,32 @@ final class IncomingEventsHandler extends AbstractEventReactor implements Loggin
                 _debug().log("Unsupported chat event type received: %s", chatEvent.getType());
                 return EitherOf4.withD(nothing());
         }
+    }
+
+    private static BotRemovedFromSpace botRemovedFromSpace(ChatEvent chatEvent) {
+        var space = chatEvent.getSpace();
+        return BotRemovedFromSpace
+                .newBuilder()
+                .setEvent(chatEvent)
+                .setSpaceId(space(space.getName()))
+                .vBuild();
+    }
+
+    private static BotAddedToSpace botAddedToSpace(ChatEvent chatEvent) {
+        var space = chatEvent.getSpace();
+        return BotAddedToSpace
+                .newBuilder()
+                .setEvent(chatEvent)
+                .setSpaceId(space(space.getName()))
+                .vBuild();
+    }
+
+    private static MessageReceived messageReceived(ChatEvent chatEvent) {
+        var message = chatEvent.getMessage();
+        return MessageReceived
+                .newBuilder()
+                .setEvent(chatEvent)
+                .setMessageId(message(message.getName()))
+                .vBuild();
     }
 }
